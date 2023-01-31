cdadfe996b0411f9f98fc8792dfcd26994e75eb7
Oleg <ovasilyevfx@mail.ru>
2023-01-31 14:46:39 +0300

    feat:Т39 Релизация классов User и Film + pom

--- src/test/java/ru/yandex/practicum/javafilmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/javafilmorate/UserControllerTest.java
@@ -3,7 +3,10 @@ package ru.yandex.practicum.javafilmorate;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.javafilmorate.controller.Controller;
+import ru.yandex.practicum.javafilmorate.controller.UserController;
 import ru.yandex.practicum.javafilmorate.custom.LocalDateAdapter;
+import ru.yandex.practicum.javafilmorate.exception.*;
 import ru.yandex.practicum.javafilmorate.model.User;
 import ru.yandex.practicum.javafilmorate.util.Client;
 
@@ -11,7 +14,10 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpResponse;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
+//import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static ru.yandex.practicum.javafilmorate.model.User.testUser;
@@ -19,6 +25,7 @@ import static ru.yandex.practicum.javafilmorate.model.User.testUser;
 class UserControllerTest {
     URI uri = URI.create("http://localhost:8080/users");
     Client client = new Client();
+    Controller userController = new UserController();
 
 
     Gson gson = new GsonBuilder()
@@ -28,83 +35,93 @@ class UserControllerTest {
 
 
     @Test
-    void findAll() throws IOException, InterruptedException {
+    void findAll() throws IOException, InterruptedException, FilmFailReleaseDate, UserCreateFailLogin, FilmWithEmptyName, CreateUserWithEmptyName, UserCreateFailEmail, FilmFailDurationNegative, UserCreateFailBirthday, UserAlreadyExist, FilmEmptyName, UserUpdateUnknown, InvalidEmailException {
+        userController.deleteAll();
         User userGetAll = testUser();
         userGetAll.setId(998);
-        String userSerialized = gson.toJson(userGetAll);
-        client.http("POST", uri, userSerialized);
-        HttpResponse<String> response = client.http("GET", uri, null);
-        assertTrue(response.statusCode() == 200, "Список пользователей не поступил");
+        userController.create(userGetAll);
+        System.out.println(userController.findAll().size());
+        assertEquals(1, userController.findAll().size(), "Длина списка пользователей не  равна 1");
     }
 
 
     @Test
-    void update() throws IOException, InterruptedException {
+    void update() throws FilmFailReleaseDate, UserCreateFailLogin, FilmWithEmptyName, CreateUserWithEmptyName,
+            UserCreateFailEmail, FilmFailDurationNegative, UserCreateFailBirthday, UserAlreadyExist, FilmEmptyName,
+            UserUpdateUnknown, InvalidEmailException, FilmUpdateUnknown {
 
-//        client.http("DELETE", uri, null);
+        userController.deleteAll();
         User userCreate = testUser();
-        String userSerialized = gson.toJson(userCreate);
-        HttpResponse<String> response = client.http("POST", uri, userSerialized);
-        int id = gson.fromJson(response.body(), User.class).getId();
+        int id = userCreate.getId();
+        userController.create(userCreate);
 
         User userUpdate = testUser();
+        String userUpdateName = userUpdate.getName();
         userUpdate.setId(id);
-        userSerialized = gson.toJson(userUpdate);
-        response = client.http("PUT", uri, userSerialized);
-        assertEquals(200, response.statusCode(), "Обновление пользователя прошло неудачно");
+        userController.update(userUpdate);
+        ArrayList<User> users = (ArrayList<User>) userController.findAll().stream().collect(Collectors.toList());
+        assertEquals(userUpdateName, users.get(0).getName(), "Обновление пользователя прошло неудачно");
 
-        client.http("DELETE", uri, null);
+        userController.deleteAll();
         User userUpdateUnknown = testUser();
         userUpdateUnknown.setId(999);
-        userSerialized = gson.toJson(userUpdateUnknown);
-        response = client.http("PUT", uri, userSerialized);
-        assertEquals(500, response.statusCode(), "Пользователь ошибочно включен в список пользователей");
+        userController.update(userUpdateName);
+        assertEquals(0, userController.findAll().size(), "Пользователь ошибочно включен в список пользователей");
+
 
     }
 
     @Test
-    void create() throws IOException, InterruptedException {
+    void create() throws IOException, InterruptedException, FilmFailReleaseDate, UserCreateFailLogin, FilmWithEmptyName, CreateUserWithEmptyName, UserCreateFailEmail, FilmFailDurationNegative, UserCreateFailBirthday, UserAlreadyExist, FilmEmptyName, UserUpdateUnknown, InvalidEmailException {
+        userController.deleteAll();
         User userCreate = testUser();
-//        client.http("DELETE", uri, null);
-        String userSerialized = gson.toJson(userCreate);
-        HttpResponse<String> response = client.http("POST", uri, userSerialized);
-        assertTrue(response.statusCode() == 200, "Пост не загрузился");
+        try {
+            userController.create(userCreate);
+        } catch (Exception e) {
+            assertEquals(1, userController.findAll().size(), "Пользователь не создался");
+        }
 
 
         User userFallLogin = testUser();
         userFallLogin.setLogin("dolore ullamco");
-        userSerialized = gson.toJson(userFallLogin);
-        response = client.http("POST", uri, userSerialized);
-        assertTrue(response.statusCode() == 500,"Пост ошибочно загрузился"
-                                                           + " с неверным логином");
+        try {
+            userController.create(userFallLogin);
+        } catch (Exception e) {
+            assertEquals(1, userController.findAll().size(), "Пост ошибочно загрузился"
+                    + " с неверным логином");
+        }
 
+
+        userController.deleteAll();
         User userFallEmail = testUser();
         userFallEmail.setEmail("mail.ru");
-        client.http("DELETE", uri, null);
-        userSerialized = gson.toJson(userFallEmail);
-        response = client.http("POST", uri, userSerialized);
-        assertTrue(response.statusCode() == 500,"Пост ошибочно загрузился с "
-                                                           + " неверным email");
+        try {
+            userController.create(userFallEmail);
+        } catch (Exception e) {
+            assertEquals(0, userController.findAll().size(), "Пост ошибочно загрузился с "
+                    + " неверным email");
+        }
 
+        userController.deleteAll();
         User userFallBirthday = testUser();
-        userFallBirthday.setBirthday(LocalDate.of(2446, 8,20));
-        client.http("DELETE", uri, null);
-        userSerialized = gson.toJson(userFallBirthday);
-        response = client.http("POST", uri, userSerialized);
+        userFallBirthday.setBirthday(LocalDate.of(2446, 8, 20));
+        try {
+            userController.create(userFallBirthday);
+        } catch (Exception e) {
+            assertEquals(0, userController.findAll().size(), "Пост ошибочно "
+                    + "загрузился с неверной датой рождения");
+        }
 
-        assertTrue(response.statusCode() == 500,"Пост ошибочно "
-                + "загрузился с неверной датой рождения"
-                                                                                        + " неверной датой рождения");
-
+        userController.deleteAll();
         User userWithEmptyName = testUser();
         userWithEmptyName.setName(null);
-        client.http("DELETE", uri, null);
-        userSerialized = gson.toJson(userWithEmptyName);
-        response = client.http("POST", uri, userSerialized);
-        assertEquals(200, response.statusCode(), "Пользователь c"
-               + " незаполненной графой 'имя пользователя'"
-               + " ошибочно включен в список пользователей");
-        client.http("DELETE", uri, null);
+        try {
+            userController.create(userWithEmptyName);
+        } catch (Exception e) {
+            assertEquals(0, userController.findAll().size(), "Пользователь c"
+                    + " незаполненной графой 'имя пользователя'"
+                    + " ошибочно включен в список пользователей");
+        }
     }
 
 }
