8aef9efd71b72ed5934ee22a57db9a4bccaff58a
Oleg <ovasilyevfx@mail.ru>
2023-02-01 11:29:31 +0300

    feat:Т39 Релизация классов User и Film + pom

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/UserExceptionUpdate.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/UserExceptionUpdate.java
@@ -3,13 +3,14 @@ package ru.yandex.practicum.javafilmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.javafilmorate.exception.InvalidEmail;
 import ru.yandex.practicum.javafilmorate.exception.UserUpdateUnknown;
+import ru.yandex.practicum.javafilmorate.exception.ValidationException;
 import ru.yandex.practicum.javafilmorate.model.User;
 
 import java.util.HashMap;
 
 @Slf4j
 public class UserExceptionUpdate {
-    public boolean update(HashMap<Integer, User> users, User user) throws UserUpdateUnknown {
+    public boolean update(HashMap<Integer, User> users, User user) throws UserUpdateUnknown, ValidationException {
 
         try {
             if (user.getEmail().isEmpty()) {
@@ -26,7 +27,7 @@ public class UserExceptionUpdate {
             }
         }
 
-        throw new UserUpdateUnknown("Пользователь " + user.getName() + " неизвестен");
+        throw new ValidationException("Пользователь " + user.getName() + " неизвестен");
     }
 
 }
