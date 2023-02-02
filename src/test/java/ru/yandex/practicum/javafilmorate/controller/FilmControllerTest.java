d3f9980c159035622772086cdfb43b82633b7c1a
Oleg <ovasilyevfx@mail.ru>
2023-02-02 14:43:08 +0300

    feat:Т39 Релизация классов User и Film + pom

--- src/test/java/ru/yandex/practicum/javafilmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/javafilmorate/controller/FilmControllerTest.java
@@ -2,13 +2,30 @@ package ru.yandex.practicum.javafilmorate.controller;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.javafilmorate.model.Film;
+import ru.yandex.practicum.javafilmorate.model.User;
 import ru.yandex.practicum.javafilmorate.service.Manager;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
     Manager<Film> manager = new Manager<>();
     FilmController filmController =new FilmController();
+
+@Test
+    void findAll()  {
+        manager.deleteAll();
+        Film filmGetAll = new Film().testFilm();
+        filmGetAll.setId(998L);
+        try {
+            filmController.create(filmGetAll);
+        }catch (Exception e) {
+            assertEquals(1, manager.findAll().size(), "Длина списка пользователей не  равна 1");
+        }
+    }
 
     @Test
     void create() {
@@ -44,24 +61,56 @@ class FilmControllerTest {
                     + " с описанием боле 200 символов");
         }
 
+        manager.deleteAll();
+        Film filmReleaseDate = new Film().testFilm();
+        filmReleaseDate.setReleaseDate(LocalDate.of(1890,3,25));
+        try {
+            filmController.create(filmReleaseDate);
+        }catch (Exception e){
+            assertEquals(0,manager.findAll().size(), "В список фильмов ошибочно вставлен фильм"
+                    + " с датой выхода раньше 1890-03-25");
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        manager.deleteAll();
+        Film filmDuration = new Film().testFilm();
+        filmDuration.setDuration(0);
+        try {
+            filmController.create(filmDuration);
+        }catch (Exception e){
+            assertEquals(0,manager.findAll().size(),"В список фильмов ошибочно вставлен фильм"
+                    + " с отрицательным временем");
+        }
     }
 
     @Test
     void update() {
+
+        manager.deleteAll();
+        Film filmCreate = new Film().testFilm();
+        Long id = filmCreate.getId();
+        try {
+            filmController.create(filmCreate);
+        }catch (Exception e){}
+
+        Film filmUpdate = new Film().testFilm();
+        String userUpdateName = filmUpdate.getName();
+        filmUpdate.setId(id);
+        ArrayList<Film> films = (ArrayList<Film>) manager.findAll().stream().collect(Collectors.toList());
+        try {
+            filmController.update(filmUpdate);
+        }catch (Exception e) {
+            assertEquals(userUpdateName, films.get(0).getName(), "Обновление фильма прошло не успешно ");
+        }
+
+        manager.deleteAll();
+        Film filmUpdateUnknown = new Film().testFilm();
+        filmUpdateUnknown.setId(10000L);
+        try {
+            filmController.create(filmUpdateUnknown);
+        }catch (Exception e) {
+            assertEquals(0, manager.findAll().size(), "Не получилось обновить  фили с дефлектором");
+        }
+
 
     }
 }
